b46e6d0a12edc332934c441f75e70840ba33d02a
Brian C Sparks <briancsparks@gmail.com>
2022-08-22 20:52:30 -0700

    Refactoring

--- ript/cheat.go
+++ ript/cheat.go
@@ -3,438 +3,339 @@ package ript
 /* Copyright © 2022 Brian C Sparks <briancsparks@gmail.com> -- MIT (see LICENSE file) */
 
 import (
-  "archive/tar"
-  "bufio"
-  "embed"
-  "fmt"
-  "github.com/spf13/pflag"
-  "io"
-  "io/fs"
-  "log"
-  "os"
-  "path/filepath"
-  "strings"
-  "syscall"
+	"archive/tar"
+	"bufio"
+	"embed"
+	"fmt"
+	"github.com/spf13/pflag"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"syscall"
 )
 
 //go:embed all:templates
 var templates embed.FS
 
 func Cheat(tname, destDir string) error {
-  if ConfigLogApis() {
-    fmt.Printf("API(Cheat): tname: %v, dest: %s\n", tname, destDir)
-  }
+	if ConfigLogApis() {
+		fmt.Printf("API(Cheat): tname: %v, dest: %s\n", tname, destDir)
+	}
 
-  //srcDir := "/home/sparksb/go/src/bcs/tryouts/__go-project-template/one"
-  srcDir := filepath.Join(myDirname, "templates", tname)
-  return Cheat2(srcDir, destDir, tname)
+	//srcDir := "/home/sparksb/go/src/bcs/tryouts/__go-project-template/one"
+	srcDir := filepath.Join(myDirname, "templates", tname)
+	return Cheat2(srcDir, destDir, tname)
 }
 
 func Cheat2(srcDir, destDir, tname string) error {
-  if ConfigLogApis() {
-    fmt.Printf("API(Cheat2): srcDir: %v, destDir: %s\n", srcDir, destDir)
-  }
+	if ConfigLogApis() {
+		fmt.Printf("API(Cheat2): srcDir: %v, destDir: %s\n", srcDir, destDir)
+	}
 
-  var nocopy map[string]string
-  var keys []string
-  var envkeys map[string]string
+	var nocopy map[string]string
+	var keys []string
+	var envkeys map[string]string
 
-  riptfilename := filepath.Join(srcDir, "riptfile.yaml")
-  nocopy, keys, envkeys, missingenv, err := readRiptfile(riptfilename)
-  if err != nil {
-    return err
-  }
+	riptfilename := filepath.Join(srcDir, "riptfile.yaml")
+	nocopy, keys, envkeys, missingenv, err := readRiptfile(riptfilename)
+	if err != nil {
+		return err
+	}
 
-  if len(missingenv) > 0 {
-    fmt.Printf("Missing ENVs: %v\n", missingenv)
-    return fmt.Errorf("Missing ENVs: %v\n", missingenv)
-  }
+	if len(missingenv) > 0 {
+		fmt.Printf("Missing ENVs: %v\n", missingenv)
+		return fmt.Errorf("Missing ENVs: %v\n", missingenv)
+	}
 
-  nocopy["riptfile.yaml"] = "riptfile.yaml"
+	nocopy["riptfile.yaml"] = "riptfile.yaml"
 
-  _, _, _ = nocopy, keys, envkeys
+	_, _, _ = nocopy, keys, envkeys
 
-  // TODO: Check that all keys and envkeys have associated values           ------
-  var flagSet = pflag.NewFlagSet("ript", pflag.ContinueOnError)
-  _ = flagSet
+	// TODO: Check that all keys and envkeys have associated values           ------
+	var flagSet = pflag.NewFlagSet("ript", pflag.ContinueOnError)
+	_ = flagSet
 
-  var args map[string]*string = make(map[string]*string)
-  for _, key := range keys {
-    args[key] = flagSet.String(key, "", "")
-  }
-  args["dest"] = flagSet.String("dest", "", "")
+	var args map[string]*string = make(map[string]*string)
+	for _, key := range keys {
+		args[key] = flagSet.String(key, "", "")
+	}
+	args["dest"] = flagSet.String("dest", "", "")
 
-  err = flagSet.Parse(os.Args[1:])
-  if err != nil {
-    return err
-  }
-  var replacements map[string]string = envkeys
-  for key, value := range args {
-    if len(*value) == 0 {
-      fmt.Printf("Missing arg: --%s=%s\n", key, key)
-      return fmt.Errorf("Missing arg: %s\n", key)
-    }
-    replacements[fmt.Sprintf("ript%s", key)] = *value
-  }
-  //                                                                        --------------------------
+	err = flagSet.Parse(os.Args[1:])
+	if err != nil {
+		return err
+	}
+	var replacements map[string]string = envkeys
+	for key, value := range args {
+		if len(*value) == 0 {
+			fmt.Printf("Missing arg: --%s=%s\n", key, key)
+			return fmt.Errorf("Missing arg: %s\n", key)
+		}
+		replacements[fmt.Sprintf("ript%s", key)] = *value
+	}
+	//                                                                        --------------------------
 
-  if ConfigUseVersion() == 1 {
-    return cheat3(srcDir, destDir, tname, nocopy, replacements)
-  }
+	if IsProd() {
+		return cheatTarfile(srcDir, destDir, tname, nocopy, replacements)
+	}
 
-  filenames := map[string]string{}
-  dirnames := map[string]string{}
+	// ================================================
+	// Active Development :)
 
-  srcFS := os.DirFS(srcDir)
-  root := "."
-  //srcFS := templates
-  //root := "templates/" + tname
+	filenames := map[string]string{}
+	dirnames := map[string]string{}
 
-  if ConfigVerbose() {
-    fmt.Printf("srcDir: %v\n", srcDir)
-    fmt.Printf("destDir: %v\n", destDir)
-    fmt.Printf("root: %v\n", root)
-  }
+	srcFS := os.DirFS(srcDir)
+	root := "."
 
-  err = fs.WalkDir(srcFS, root, func(shortPath string, d fs.DirEntry, err error) error {
-    if ConfigVerbose() {
-      fmt.Printf("shortPath: %v\n", shortPath)
-    }
+	if ConfigVerbose() {
+		fmt.Printf("srcDir: %v\n", srcDir)
+		fmt.Printf("destDir: %v\n", destDir)
+		fmt.Printf("root: %v\n", root)
+	}
 
-    if d == nil {
-      return err
-    }
+	err = fs.WalkDir(srcFS, root, func(shortPath string, d fs.DirEntry, err error) error {
+		if ConfigVerbose() {
+			fmt.Printf("shortPath: %v\n", shortPath)
+		}
 
-    fi, err := d.Info()
-    _, _ = fi, err
+		if d == nil {
+			return err
+		}
 
-    // Save a bunch of work
-    if d.IsDir() {
-      if shortPath == "." {
-        return nil
-      }
-      if shortPath == ".git" || shortPath == "node_modules" || shortPath == ".idea" {
-        return fs.SkipDir
-      }
-    } else {
-      _, present := nocopy[shortPath]
-      if present {
-        return nil
-      }
-    }
+		fi, err := d.Info()
+		_, _ = fi, err
 
-    // Figure out the destination dirs, and make them - make sure to rename things when necessary
-    shortDestPath := shortPath
-    for oldStr, newStr := range replacements {
-      shortDestPath = strings.ReplaceAll(shortDestPath, oldStr, newStr)
-    }
+		// Save a bunch of work
+		if d.IsDir() {
+			if shortPath == "." {
+				return nil
+			}
+			if shortPath == ".git" || shortPath == "node_modules" || shortPath == ".idea" {
+				return fs.SkipDir
+			}
+		} else {
+			_, present := nocopy[shortPath]
+			if present {
+				return nil
+			}
+		}
 
-    srcPath := filepath.Clean(filepath.Join(srcDir, shortPath))
-    destPath := filepath.Clean(filepath.Join(destDir, shortDestPath))
-    //fmt.Printf("srcPath: %v, destPath: %v\n", srcPath, destPath)
+		// Figure out the destination dirs, and make them - make sure to rename things when necessary
+		shortDestPath := shortPath
+		for oldStr, newStr := range replacements {
+			shortDestPath = strings.ReplaceAll(shortDestPath, oldStr, newStr)
+		}
 
-    //info, _ := d.Info()
-    //fmt.Printf("%s: dirent: %v; err: %v\n", shortPath, info, err)
+		srcPath := filepath.Clean(filepath.Join(srcDir, shortPath))
+		destPath := filepath.Clean(filepath.Join(destDir, shortDestPath))
+		//fmt.Printf("srcPath: %v, destPath: %v\n", srcPath, destPath)
 
-    if ConfigUseVersion() == 2 {
-      if d.IsDir() {
-        if ConfigClobber() {
-          err = os.MkdirAll(destPath, os.FileMode(0755)) // TODO: Magic number
-        }
-      } else {
-        //if ConfigClobber() {
-        srcFd, err := os.Open(srcPath)
-        if err != nil {
-          log.Panic(err)
-        }
-        putFileTotallyBroken(destPath, srcPath, srcFd, replacements, 0644) // TODO: Magic number
-        //}
-      }
+		//info, _ := d.Info()
+		//fmt.Printf("%s: dirent: %v; err: %v\n", shortPath, info, err)
 
-    } else {
-      if d.IsDir() {
-        dirnames[srcPath] = destPath
-      } else {
-        filenames[srcPath] = destPath
-      }
-    }
+		if d.IsDir() {
+			dirnames[srcPath] = destPath
+		} else {
+			filenames[srcPath] = destPath
+		}
 
-    return nil
-  })
-  if err != nil {
-    log.Panic(err)
-  }
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
 
-  if ConfigVerbose() {
-    fmt.Printf("dirnames:\n%v\n", joinMap(&dirnames))
-    fmt.Printf("filenames:\n%v\n", joinMap(&filenames))
-  }
+	if ConfigVerbose() {
+		fmt.Printf("dirnames:\n%v\n", joinMap(&dirnames))
+		fmt.Printf("filenames:\n%v\n", joinMap(&filenames))
+	}
 
-  if ConfigUseVersion() == 0 {
-    if ConfigClobber() {
-      // Make dest dirs
-      _ = syscall.Umask(0)
-      for _, dirname := range dirnames {
-        err := os.MkdirAll(dirname, os.FileMode(0755)) // TODO: Magic number
-        if err != nil && !os.IsExist(err) {
-          log.Panic(err)
-        }
-      }
-    }
+	if ConfigClobber() {
+		// Make dest dirs
+		_ = syscall.Umask(0)
+		for _, dirname := range dirnames {
+			err := os.MkdirAll(dirname, os.FileMode(0755)) // TODO: Magic number
+			if err != nil && !os.IsExist(err) {
+				log.Panic(err)
+			}
+		}
+	}
 
-    if ConfigClobber() {
-      // Copy files
-      for src, dest := range filenames {
-        func(src, dest string) {
-          srcFd, err := os.Open(src)
-          if err != nil {
-            log.Panic(err)
-          }
-          defer srcFd.Close()
+	if ConfigClobber() {
+		// Copy files
+		for src, dest := range filenames {
+			func(src, dest string) {
+				srcFd, err := os.Open(src)
+				if err != nil {
+					log.Panic(err)
+				}
+				defer srcFd.Close()
 
-          //destFd, err := os.Create(dest)
-          destFd, err := os.OpenFile(dest, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) // TODO: Magic number
-          if err != nil {
-            log.Panic(err)
-          }
-          defer destFd.Close()
+				//destFd, err := os.Create(dest)
+				destFd, err := os.OpenFile(dest, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) // TODO: Magic number
+				if err != nil {
+					log.Panic(err)
+				}
+				defer destFd.Close()
 
-          datawriter := bufio.NewWriter(destFd)
+				datawriter := bufio.NewWriter(destFd)
 
-          scanner := bufio.NewScanner(srcFd)
-          for scanner.Scan() {
-            line := scanner.Text()
-            line = replaceEmAll(line, replacements)
-            datawriter.WriteString(line + "\n")
-          }
-          if err := scanner.Err(); err != nil {
-            log.Panic(err)
-          }
-          datawriter.Flush()
+				scanner := bufio.NewScanner(srcFd)
+				for scanner.Scan() {
+					line := scanner.Text()
+					line = replaceEmAll(line, replacements)
+					datawriter.WriteString(line + "\n")
+				}
+				if err := scanner.Err(); err != nil {
+					log.Panic(err)
+				}
+				datawriter.Flush()
 
-        }(src, dest)
-      }
-    }
-  }
+			}(src, dest)
+		}
+	}
 
-  return nil
+	return nil
 }
 
-func putFileTotallyBroken(destName, srcName string, srcFd io.ReadCloser, replacements map[string]string, perm os.FileMode) {
-  defer srcFd.Close()
+//func putFileTotallyBroken(destName, srcName string, srcFd io.ReadCloser, replacements map[string]string, perm os.FileMode) {
+//  defer srcFd.Close()
+//
+//  dest := replaceEmAll(srcName, replacements)
+//  dest = destName
+//
+//  if ConfigClobber() {
+//    func(srcFd io.Reader, dest string, perm os.FileMode) {
+//      destFd, err := os.OpenFile(dest, os.O_APPEND|os.O_CREATE|os.O_WRONLY, perm)
+//      if err != nil {
+//        log.Panic(err)
+//      }
+//      defer destFd.Close()
+//
+//      datawriter := bufio.NewWriter(destFd)
+//
+//      linelen := 0
+//      linenum := 0
+//      var line string
+//      scanner := bufio.NewScanner(srcFd)
+//      for scanner.Scan() {
+//        linenum += 1
+//        line = scanner.Text()
+//        linelen = len(line)
+//        line = replaceEmAll(line, replacements)
+//        count, err := datawriter.WriteString(line + "\n")
+//        if err != nil {
+//          log.Panic(err)
+//        }
+//        fmt.Printf("%04d count: %v %v %s", linenum, linelen, count, line)
+//      }
+//      if err := scanner.Err(); err != nil {
+//        log.Panic(err)
+//      }
+//      datawriter.Flush()
+//    }(srcFd, dest, perm)
+//
+//    //replacer := NewReplacer(src, replacements)
+//    //count, err := io.Copy(destFd, replacer)
+//    //if err != nil {
+//    //	log.Panic(err)
+//    //}
+//    //_ = count
+//  }
+//}
 
-  dest := replaceEmAll(srcName, replacements)
-  dest = destName
+func cheatTarfile(srcDir, destDir, tname string, nocopy map[string]string, replacements map[string]string) error {
+	_, _, _, _, _ = srcDir, destDir, tname, nocopy, replacements
 
-  if ConfigClobber() {
-    func(srcFd io.Reader, dest string, perm os.FileMode) {
-      destFd, err := os.OpenFile(dest, os.O_APPEND|os.O_CREATE|os.O_WRONLY, perm)
-      if err != nil {
-        log.Panic(err)
-      }
-      defer destFd.Close()
+	srcFS := templates
 
-      datawriter := bufio.NewWriter(destFd)
+	tarFilename := filepath.Join("templates", tname+".tar")
 
-      linelen := 0
-      linenum := 0
-      var line string
-      scanner := bufio.NewScanner(srcFd)
-      for scanner.Scan() {
-        linenum += 1
-        line = scanner.Text()
-        linelen = len(line)
-        line = replaceEmAll(line, replacements)
-        count, err := datawriter.WriteString(line + "\n")
-        if err != nil {
-          log.Panic(err)
-        }
-        fmt.Printf("%04d count: %v %v %s", linenum, linelen, count, line)
-      }
-      if err := scanner.Err(); err != nil {
-        log.Panic(err)
-      }
-      datawriter.Flush()
-    }(srcFd, dest, perm)
+	tarfile, err := srcFS.Open(tarFilename)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer tarfile.Close()
 
-    //replacer := NewReplacer(src, replacements)
-    //count, err := io.Copy(destFd, replacer)
-    //if err != nil {
-    //	log.Panic(err)
-    //}
-    //_ = count
-  }
-}
+	tr := tar.NewReader(tarfile)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break // End of archive
+		}
+		if err != nil {
+			log.Panic(err)
+		}
+		//fi := hdr.FileInfo()
 
-func cheat3(srcDir, destDir, tname string, nocopy map[string]string, replacements map[string]string) error {
-  _, _, _, _, _ = srcDir, destDir, tname, nocopy, replacements
+		// If this is the root, skip
+		if hdr.Name == tname+"/" {
+			continue
+		}
 
-  //srcFS := os.DirFS(srcDir)
-  //root := "."
-  srcFS := templates
-  //root := "templates/" + tname
+		pathname := filepath.Join(destDir, trimPrefix(hdr.Name, tname+"/"))
+		pathname = replaceEmAll(pathname, replacements)
 
-  tarFilename := filepath.Join("templates", tname+".tar")
+		if isDir(hdr) {
+			err = os.MkdirAll(pathname, os.FileMode(hdr.Mode))
+			if err != nil {
+				log.Panic(err)
+			}
+			continue
+		}
 
-  tarfile, err := srcFS.Open(tarFilename)
-  if err != nil {
-    log.Panic(err)
-  }
-  defer tarfile.Close()
+		_, present := nocopy[filepath.Base(hdr.Name)]
+		if present {
+			continue
+		}
 
-  tr := tar.NewReader(tarfile)
-  for {
-    hdr, err := tr.Next()
-    if err == io.EOF {
-      break // End of archive
-    }
-    if err != nil {
-      log.Panic(err)
-    }
-    fi := hdr.FileInfo()
+		if ConfigClobber() {
+			// Copy files
+			func(dest string) {
 
-    // If this is the root, skip
-    if hdr.Name == tname+"/" {
-      continue
-    }
+				destFd, err := os.OpenFile(dest, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.FileMode(hdr.Mode))
+				if err != nil {
+					log.Panic(err)
+				}
+				defer destFd.Close()
 
-    pathname := filepath.Join(destDir, trimPrefix(hdr.Name, tname+"/"))
-    pathname = replaceEmAll(pathname, replacements)
+				datawriter := bufio.NewWriter(destFd)
 
-    if isDir(hdr) {
-      err = os.MkdirAll(pathname, os.FileMode(hdr.Mode))
-      if err != nil {
-        log.Panic(err)
-      }
-      continue
-    }
+				scanner := bufio.NewScanner(tr)
+				for scanner.Scan() {
+					line := scanner.Text()
+					line = replaceEmAll(line, replacements)
+					datawriter.WriteString(line + "\n")
+				}
+				if err := scanner.Err(); err != nil {
+					log.Panic(err)
+				}
+				datawriter.Flush()
 
-    _, present := nocopy[filepath.Base(hdr.Name)]
-    if present {
-      continue
-    }
+			}(pathname)
+		}
+		//_, _ = hdr, fi
+	}
 
-    if ConfigClobber() {
-      // Copy files
-      //for src, dest := range filenames {
-      func(dest string) {
-        //srcFd, err := os.Open(src)
-        //if err != nil {
-        //  log.Panic(err)
-        //}
-        //defer srcFd.Close()
-
-        //destFd, err := os.Create(dest)
-        destFd, err := os.OpenFile(dest, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.FileMode(hdr.Mode)) // TODO: Magic number
-        if err != nil {
-          log.Panic(err)
-        }
-        defer destFd.Close()
-
-        datawriter := bufio.NewWriter(destFd)
-
-        scanner := bufio.NewScanner(tr)
-        for scanner.Scan() {
-          line := scanner.Text()
-          line = replaceEmAll(line, replacements)
-          datawriter.WriteString(line + "\n")
-        }
-        if err := scanner.Err(); err != nil {
-          log.Panic(err)
-        }
-        datawriter.Flush()
-
-      }(pathname)
-      //}
-    }
-
-    //replacer := NewReplacer(tr, replacements)
-    //_, _, _ = hdr, replacer, fi
-
-    _, _ = hdr, fi
-  }
-
-  //if ConfigVerbose() {
-  //  fmt.Printf("srcDir: %v\n", srcDir)
-  //  fmt.Printf("destDir: %v\n", destDir)
-  //  fmt.Printf("root: %v\n", root)
-  //}
-
-  //foundTarfile := false
-  //err = fs.WalkDir(srcFS, root, func(shortPath string, d fs.DirEntry, err error) error {
-  //  if ConfigVerbose() {
-  //    fmt.Printf("shortPath: %v\n", shortPath)
-  //  }
-  //  if d == nil {
-  //    return err
-  //  }
-  //
-  //  if shortPath == tarFilename {
-  //    foundTarfile = true
-  //
-  //    //tarBytes := d.
-  //
-  //    return nil
-  //  }
-  //
-  //  if d.IsDir() && foundTarfile {
-  //    return filepath.SkipDir
-  //  }
-  //
-  //  //// Save a bunch of work
-  //  //if d.IsDir() {
-  //  //	if shortPath == "." {
-  //  //		return nil
-  //  //	}
-  //  //	if shortPath == ".git" || shortPath == "node_modules" || shortPath == ".idea" {
-  //  //		return fs.SkipDir
-  //  //	}
-  //  //} else {
-  //  //	_, present := nocopy[shortPath]
-  //  //	if present {
-  //  //		return nil
-  //  //	}
-  //  //}
-  //  //
-  //  //// Figure out the destination dirs, and make them - make sure to rename things when necessary
-  //  //shortDestPath := shortPath
-  //  //for oldStr, newStr := range replacements {
-  //  //	shortDestPath = strings.ReplaceAll(shortDestPath, oldStr, newStr)
-  //  //}
-  //
-  //  //srcPath := filepath.Clean(filepath.Join(srcDir, shortPath))
-  //  //destPath := filepath.Clean(filepath.Join(destDir, shortDestPath))
-  //  //fmt.Printf("srcPath: %v, destPath: %v\n", srcPath, destPath)
-  //
-  //  //info, _ := d.Info()
-  //  //fmt.Printf("%s: dirent: %v; err: %v\n", shortPath, info, err)
-  //
-  //  //if d.IsDir() {
-  //  //  dirnames[srcPath] = destPath
-  //  //} else {
-  //  //  filenames[srcPath] = destPath
-  //  //}
-  //  return nil
-  //})
-  //if err != nil {
-  //  log.Panic(err)
-  //}
-
-  return nil
+	return nil
 }
 
 func isDir(hdr *tar.Header) bool {
-  if strings.HasSuffix(hdr.Name, "/") {
-    return true
-  }
-  return false
+	if strings.HasSuffix(hdr.Name, "/") {
+		return true
+	}
+	return false
 }
 
 func replaceEmAll(str string, replacements map[string]string) string {
-  result := str
-  for oldStr, newStr := range replacements {
-    result = strings.ReplaceAll(result, oldStr, newStr)
-  }
+	result := str
+	for oldStr, newStr := range replacements {
+		result = strings.ReplaceAll(result, oldStr, newStr)
+	}
 
-  return result
+	return result
 }
--- ript/config.go
+++ ript/config.go
@@ -5,67 +5,83 @@ package ript
 //go:generate go run genconfig.go
 
 type Config struct {
-	Dryrun       bool
-	NoClobber    bool
-	Verbose      bool
-	LogApis      bool
-	VersionToUse int
+  Dryrun       bool
+  NoClobber    bool
+  Verbose      bool
+  LogApis      bool
+  VersionToUse int
+
+  ActiveD bool
 }
 
 var theConfig *Config
 
 func init() {
-	theConfig = &Config{}
+  theConfig = &Config{}
 
-	//theConfig.NoClobber = true
-	//theConfig.Verbose = true
-	//theConfig.LogApis = true
-	//theConfig.VersionToUse = 0 // 0 == orig
-	theConfig.VersionToUse = 1 // 1 == tarfile
-	//theConfig.VersionToUse = 2 // 2 == old, but in walk callback
+  theConfig.ActiveD = true
+
+  //theConfig.NoClobber = true
+  //theConfig.Verbose = true
+  //theConfig.LogApis = true
+  //theConfig.VersionToUse = 0 // 0 == orig
+  theConfig.VersionToUse = 1 // 1 == tarfile
+  //theConfig.VersionToUse = 2 // 2 == old, but in walk callback
 }
 
 func NewConfig(dryrun bool) *Config {
-	return &Config{
-		Dryrun: dryrun,
-	}
+  return &Config{
+    Dryrun: dryrun,
+  }
 }
 
 func (c *Config) SetSafe(safe bool) {
-	c.NoClobber = safe
+  c.NoClobber = safe
 }
 
 func (c *Config) SetVerbose(verbose bool) {
-	c.Verbose = verbose
+  c.Verbose = verbose
 }
 
 func ConfigDryRun() bool {
-	return theConfig.Dryrun
+  return theConfig.Dryrun
 }
 
 func ConfigNoClobber() bool {
-	return theConfig.NoClobber
+  return theConfig.NoClobber
 }
 
 func ConfigClobber() bool {
-	return !theConfig.NoClobber
+  return !theConfig.NoClobber
 }
 
 func ConfigVerbose() bool {
-	return theConfig.Verbose
+  return theConfig.Verbose
 }
 
 func ConfigLogApis() bool {
-	return theConfig.LogApis
+  return theConfig.LogApis
 }
 
 func ConfigUseVersion() int {
-	return theConfig.VersionToUse
+  return theConfig.VersionToUse
+}
+
+func ConfigIsActiveD() bool {
+  return theConfig.ActiveD
+}
+
+func ActiveD() bool {
+  return ConfigIsActiveD()
+}
+
+func IsProd() bool {
+  return !ActiveD()
 }
 
 func ConfigIf(n int) bool {
-	if n == 0 {
-		return false
-	}
-	return true
+  if n == 0 {
+    return false
+  }
+  return true
 }
