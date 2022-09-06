f90db61003790ef37b3d633f3a55994697b50814
Brian C Sparks <briancsparks@gmail.com>
2022-09-06 02:26:36 +0100

    Refactoring

--- ript/templates/gocli/cmd/root.go
+++ ript/templates/gocli/cmd/root.go
@@ -11,11 +11,11 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   programRootName,
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-This CLI blah, blah, blah.`,
+	Short: "riptprojectname is an application",
+//	Long: `A longer description that spans multiple lines and likely contains
+//examples and usage of using your application. For example:
+//
+//This CLI blah, blah, blah.`,
 
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
